b3b63d3e5aaf5dbfd0bba0a7354bc4b4c615f95b
nutmi1k <dotten7792@gmail.com>
2022-10-30 20:44:28 -0500

    init

---
+++ src/main/java/com/example/oddzcrawler/OddzCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oddzcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddzCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddzCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oddzcrawler/OddzCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oddzcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddzCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
